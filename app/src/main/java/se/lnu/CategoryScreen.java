7e673ca7037db2b6aec506ed7ff7e3e76e401a1e
Muhammad Haseeb <mh227kb@student.lnu.se>
2026-04-28 12:42:30 +0200

    Maybe Final from my side

--- app/src/main/java/se/lnu/CategoryScreen.java
+++ app/src/main/java/se/lnu/CategoryScreen.java
@@ -1,8 +1,11 @@
 package se.lnu;
 
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import se.lnu.database.DatabaseHelper;
@@ -13,6 +16,24 @@ public class CategoryScreen {
 
   public static void show(Stage stage) {
 
+    // Back button
+    Button backBtn = new Button("Back");
+    backBtn.setStyle(
+            "-fx-font-size: 16px;" +
+                    "-fx-background-color: #eeeeee;" +
+                    "-fx-text-fill: #333333;" +
+                    "-fx-padding: 10 20;" +
+                    "-fx-background-radius: 10;"
+    );
+
+    backBtn.setOnAction(e -> {
+      OrderTypeScreen.show(stage);
+    });
+
+    HBox topBar = new HBox(backBtn);
+    topBar.setAlignment(Pos.CENTER_LEFT);
+
+    // category layout
     VBox layout = new VBox(15);
     layout.setAlignment(Pos.CENTER);
 
@@ -27,7 +48,6 @@ public class CategoryScreen {
               "-fx-background-radius: 15;"
       );
 
-
       btn.setOnAction(e -> {
         App.setCategory(c.getId(), c.getName());
         ItemListScreen.show(stage);
@@ -36,7 +56,13 @@ public class CategoryScreen {
       layout.getChildren().add(btn);
     }
 
-    Scene scene = new Scene(layout, 600, 400);
+    // Root layout
+    BorderPane root = new BorderPane();
+    root.setPadding(new Insets(20));
+    root.setTop(topBar);
+    root.setCenter(layout);
+
+    Scene scene = new Scene(root, 600, 400);
     stage.setScene(scene);
     stage.setTitle("Select Category");
     stage.show();
